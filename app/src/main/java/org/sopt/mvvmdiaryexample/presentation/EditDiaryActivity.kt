037dd3a8e3ae0e06f3249908b484aef0e301fcb2
KxxHyoRim <rlagyfla0709@gmail.com>
2022-03-01 23:37:23 +0900

    [FEAT] EditDiaryActivity.kt DataBinding 설정

--- app/src/main/java/org/sopt/mvvmdiaryexample/presentation/EditDiaryActivity.kt
+++ app/src/main/java/org/sopt/mvvmdiaryexample/presentation/EditDiaryActivity.kt
@@ -1,9 +1,11 @@
 package org.sopt.mvvmdiaryexample.presentation
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import org.sopt.mvvmdiaryexample.databinding.ActivityEditDiaryBinding
+import org.sopt.mvvmdiaryexample.domain.Diary
+import java.util.*
 
 class EditDiaryActivity : AppCompatActivity() {
 
@@ -11,9 +13,27 @@ class EditDiaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityEditDiaryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /**ViewBinding을 쓸거면 아래와 같이 사용*/
+
+        val diary = Diary(id = 0, title = "제목", content = "내용", createDate = Date()/*현재시간*/)
+//        val diary2 = Diary( 0, "제목", "내용",Date()/*현재시간*/)
+//        둘 다 같은 표현인데, 위에는 명시적, 아래는 암시적이라고 하는 듯 하다
+
+//        binding.tvDate.text = diary.createDate.toString()
+//        binding.tvTitle.text = diary.title
+
+        /**DataBinding을 쓸거면 아래와 같이 사용*/
+        // 방법 1
+        binding.memo = diary
+        binding.bindingString = "제목DataBinding"
+        binding.bindingInteger = 123456789  /* Int형은 xml에서 android:text="@{String.valueOf()} 처리*/
+
+
+        // 방법 2
+//        binding.setVariable(BR.diary, diary)
 
         binding.buttonSubmit.setOnClickListener {
             setResult(Activity.RESULT_OK)
