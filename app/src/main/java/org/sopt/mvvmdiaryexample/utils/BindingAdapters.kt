02f0af45438d582a1e3b7b7739957b4d41c405c7
KxxHyoRim <rlagyfla0709@gmail.com>
2022-03-19 16:42:46 +0900

    [FEAT] BindingAdapter 확장함수 형태로 변환

--- app/src/main/java/org/sopt/mvvmdiaryexample/utils/BindingAdapters.kt
+++ app/src/main/java/org/sopt/mvvmdiaryexample/utils/BindingAdapters.kt
@@ -5,14 +5,11 @@ import androidx.databinding.BindingAdapter
 import java.text.SimpleDateFormat
 import java.util.*
 
-/**
- * @BindingAdapter("createdDate"),  @BindingAdapter("app:createdDate")
- * 둘 다 사용 가능하지만, app: 을 붙이는 것이 정석적인 방법이라고 한다
- * */
-//@BindingAdapter("createdDate")
+/* @BindingAdapter("createdDate"),  @BindingAdapter("app:createdDate")
+ * 둘 다 사용 가능하지만, app: 을 붙이는 것이 정석적인 방법이라고 한다*/
 @BindingAdapter("app:createdDate")
-fun bindMemoCreatedDate(textView: TextView, date: Date?) {  //null 이 들어올 수도 있음
+fun TextView.bindMemoCreatedDate(date: Date?) {  //null 이 들어올 수도 있음
     if (date == null) return
     val simpleDateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
-    textView.text = simpleDateFormat.format(date)
+    this.text = simpleDateFormat.format(date)
 }
