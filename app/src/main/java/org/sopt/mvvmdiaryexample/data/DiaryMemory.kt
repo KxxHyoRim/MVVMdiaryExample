7f8707dd65c6e56d86fe616a5c364ed265dd0c49
KxxHyoRim <rlagyfla0709@gmail.com>
2022-03-19 10:42:24 +0900

    [ADD] DiaryMemory.kt

---
+++ app/src/main/java/org/sopt/mvvmdiaryexample/data/DiaryMemory.kt
@@ -0,0 +1,22 @@
+package org.sopt.mvvmdiaryexample.data
+
+import org.sopt.mvvmdiaryexample.domain.Diary
+import java.util.*
+
+object DiaryMemory {
+    private val diaries: MutableMap<String, Diary> = mutableMapOf(
+        "first" to Diary("first", "기본 메모", "그냥 들어있는 메모", Date())
+    )
+
+    fun saveDiary(diary: Diary) {
+        diaries[diary.id] = diary
+    }
+
+    fun getDiary(id: String): Diary {
+        return diaries[id] ?: error("cannot find diary id of $id")
+    }
+
+    fun getAllDiaries(): List<Diary> {
+        return diaries.map { it.value }
+    }
+}
