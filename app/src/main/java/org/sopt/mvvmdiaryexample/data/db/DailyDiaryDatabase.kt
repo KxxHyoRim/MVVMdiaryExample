13ef3e3f28014e3c31ea52a32991b3ee9aa28539
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-04-02 11:11:42 +0900

    [ADD] DB 관련 파일들 추가

---
+++ app/src/main/java/org/sopt/mvvmdiaryexample/data/db/DailyDiaryDatabase.kt
@@ -0,0 +1,30 @@
+package org.sopt.mvvmdiaryexample.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import org.sopt.mvvmdiaryexample.data.converter.DateTypeConverter
+import org.sopt.mvvmdiaryexample.data.dao.DiaryDao
+import org.sopt.mvvmdiaryexample.data.entity.DiaryEntity
+
+@TypeConverters(DateTypeConverter::class)
+@Database(entities = [DiaryEntity::class], version = 1)
+/** version 은 나중에 DB를 사용하다가 schema가 바뀔 경우에 수정해줌
+ * Schema Migration을 해야하는데 이때 사용함*/
+abstract class DailyDiaryDatabase : RoomDatabase() {
+
+    abstract fun getDiariesDao(): DiaryDao
+
+    companion object {
+        @JvmStatic
+        fun newInstance(context: Context): DailyDiaryDatabase {
+            return Room.databaseBuilder(
+                context,
+                DailyDiaryDatabase::class.java,
+                "DailyDiaryDatabase"
+            ).build()
+        }
+    }
+}
